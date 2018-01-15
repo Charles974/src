5512768e911b84a7d6dae9f1b25d84eb8a625380
Ragonneau <v.ragonneau@laposte.net>
2018-01-15 16:22:27 +0100

    Design conv

--- components/ConversationElement.vue
+++ components/ConversationElement.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="conv">
     <h3>{{ channel.label }}</h3>
     <p>{{ channel.topic }}</p>
   </div>
@@ -19,5 +19,25 @@ export default {
 </script>
 
 <style scoped>
+
+.conv{
+  width: 15%;
+  box-sizing: border-box;
+  background-color: #ADD8E6;
+  margin-top: 1em;
+  margin-left: 3.571428571%;
+}
+
+p{
+  width: 100%;
+  font-size: 0.8em;
+  text-align: center;
+}
+
+h3{
+  width: 100%;
+  font-size: 1em;
+  text-align: center;
+}
 
 </style>
